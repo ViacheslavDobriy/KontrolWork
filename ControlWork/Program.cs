dc3cb9986732aa3f072a9bb7abd34632522911ee
Vyacheslav <dobrov.slava@yandex.ru>
2022-02-16 21:49:28 +0300

    Added method to count numbers of words in user's line

---
+++ ControlWork/Program.cs
@@ -0,0 +1,16 @@
+﻿int HowManyWordsInLine(string line)
+{
+    int result = 1;
+    for(int i = 0; i < line.Length; i++)
+    {
+        if(line[i] == ' ')
+        {
+            result++;
+        }
+    }
+    return result; 
+}
+Console.WriteLine("Hello, User! Enter words, press 'Enter'-button and you will see a magic!");
+string UserLine = Console.ReadLine() ?? "0";
+Console.WriteLine($"There are {HowManyWordsInLine(UserLine)} words");
+
