787fb031f63693ea4f73067d0a396aebae5b7b67
Vyacheslav <dobrov.slava@yandex.ru>
2022-02-18 23:28:42 +0300

    Progamm is working, I used 3 methods for founding solution

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -10,7 +10,36 @@
     }
     return result; 
 }
+string[] CreateNewArray(string line, int numberofwords)
+{
+    string[] array = new string[numberofwords];
+    int k = 0;
+    for(int i = 0; i < line.Length; i++)
+    {
+        if(line[i]!=' ')
+        {
+            array[k] = array[k]+line[i];
+        }
+        else 
+        {
+            k++;
+        }
+    }
+    return array;
+}
+void ShowImportant(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length>=5)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+}
 Console.WriteLine("Hello, User! Enter words, press 'Enter'-button and you will see a magic!");
-string UserLine = Console.ReadLine() ?? "0";
-Console.WriteLine($"There are {HowManyWordsInLine(UserLine)} words");
+string line = Console.ReadLine() ?? "0";
+Console.WriteLine($"There are {HowManyWordsInLine(line)} words");
+string[] newArray = CreateNewArray(line, HowManyWordsInLine(line));
+ShowImportant(newArray);
 
